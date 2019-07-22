905862aa5f7266247e92bf89610d7eb76ad84cba
penadidik <di2k.skh@gmail.com>
2019-07-22 08:53:08 +0700

    version 1

---
+++ app/src/main/java/di2k/lintaspena/slidebycarousel/ImageModel.kt
@@ -0,0 +1,14 @@
+package di2k.lintaspena.slidebycarousel
+//Created by 디딬 Didik M. Hadiningrat on 21 July 2019
+class ImageModel {
+    private var image_drawable: Int = 0
+
+    fun getImage_drawables(): Int{
+        return image_drawable
+    }
+
+    fun setImage_drawables(image_drawable: Int){
+        this.image_drawable = image_drawable
+    }
+
+}
